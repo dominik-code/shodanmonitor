bb8945d06a21b8b76c93b694d024420818b9543c
dominik-code <git@dominik-schmitt.eu>
2019-05-19 19:21:16 +0200

    Added random ip scan and function

--- executeQuery.php
+++ executeQuery.php
@@ -8,7 +8,133 @@ require_once 'Tag.class.php';
 require_once 'Vuln.class.php';
 require_once 'CIDR.class.php';
 
+
+function scanIP($starttime, $ip) {
+    $runtime = microtime(true) - $starttime;
+    if($runtime > 45.0) {
+        exit("Runtime limit reached aborting scan for now <br>");
+    }
+
+
+    if (Host::exist(ip2long($ip))) {
+        echo "Host: $ip already exist skipping scan in favor of new hosts. <br>";
+        return;
+    }
+
+
+    echo "Scanning: ". $ip . "<br>";
+    usleep(500000);
+
+    $ipv4 = $ip;
+
+    $apikey = APIKEY;
+
+    $url = "https://api.shodan.io/shodan/host/$ipv4?key=$apikey&history=False&minify=True";
+
+    $ch = curl_init();
+// Will return the response, if false it print the response
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// Set the url
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+// Execute
+    $result = curl_exec($ch);
+    $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+// Closing
+    curl_close($ch);
+
+// Will dump a beauty json :3
+    $result_array = json_decode($result, true);
+
+    if ($http_status != "200") {
+        if ($http_status == "404") {
+            Host::create(ip2long($ip), $ip, null);
+            echo "No data for this ip: $ipv4 <br>";
+            return;
+        }
+        exit("Wrong HTTP Status Code: $http_status");
+    }
+
+    if (isset($result_array['error'])) {
+        // happens if "No information available for that IP."
+        exit("An error was detected.");
+    }
+
+
+    if (!isset($result_array['ip'])) {
+        // ip as integer 12345678
+        die("No IP given");
+    }
+
+
+    if (!Host::exist($result_array['ip'])) {
+        // insert if not exist
+        Host::create($result_array['ip'], $result_array['ip_str'], $result_array['last_update']);
+    }
+
+    $host = new Host($result_array['ip']);
+
+    if (isset($result_array['last_update'])) {
+        $host->updateLastUpdate($result_array['last_update']);
+    }
+
+    if (isset($result_array['ports'])) {
+        $host->updatePorts($result_array['ports']);
+    }
+
+    if (isset($result_array['vulns'])) {
+        $host->updateVulns($result_array['vulns']);
+    }
+
+    if (isset($result_array['tags'])) {
+        $host->updateTags($result_array['tags']);
+    }
+
+    if (isset($result_array['hostnames'])) {
+        $host->updateHostnames($result_array['hostnames']);
+    }
+
+    if (isset($result_array['country_code']) || isset($result_array['country_code3']) || isset($result_array['country_name'])) {
+        $host->updateCountry($result_array['country_code'], $result_array['country_code3'], $result_array['country_name']);
+    }
+
+    if (isset($result_array['os'])) {
+        $host->updateOS($result_array['os']);
+    }
+
+    if (isset($result_array['org'])) {
+        $host->updateORG($result_array['org']);
+    }
+
+    if (isset($result_array['asn'])) {
+        $host->updateASN($result_array['asn']);
+    }
+
+    if (isset($result_array['isp'])) {
+        $host->updateISP($result_array['isp']);
+    }
+
+    if (isset($result_array['latitude']) && isset($result_array['longitude'])) {
+        $host->updateLocation($result_array['latitude'], $result_array['longitude']);
+    }
+}
+
+
+
 $starttime = microtime(true);
+
+
+$randomip = "";
+$randomip .= mt_rand(1,254);
+$randomip .= ".";
+$randomip .= mt_rand(0,254);
+$randomip .= ".";
+$randomip .= mt_rand(0,254);
+$randomip .= ".";
+$randomip .= mt_rand(1,254);
+
+scanIP($starttime, $randomip);
+
 
 
 $mysqli = new mysqli(DBHOST, DBUSER, DBPASS, DATABASE);
@@ -37,123 +163,7 @@ while ($row = $res->fetch_assoc()) {
 
     $ips = CIDR::cidrToRange($cidr);
     foreach ($ips as $ip) {
-        $runtime = microtime(true) - $starttime;
-        if($runtime > 45.0) {
-            exit("Runtime limit reached aborting scan for now <br>");
-        }
-
-
-        if (Host::exist(ip2long($ip))) {
-            echo "Host: $ip already exist skipping scan in favor of new hosts. <br>";
-            continue;
-        }
-
-
-        echo "Scanning: ". $ip . "<br>";
-        usleep(500000);
-
-
-        $isValid = filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
-
-        if (§isValid == false) {
-            echo "not correct ipv4 format";
-            continue;
-        }
-
-        $ipv4 = $ip;
-
-        $apikey = APIKEY;
-
-        $url = "https://api.shodan.io/shodan/host/$ipv4?key=$apikey&history=False&minify=True";
-
-        $ch = curl_init();
-// Will return the response, if false it print the response
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// Set the url
-        curl_setopt($ch, CURLOPT_URL, $url);
-
-// Execute
-        $result = curl_exec($ch);
-        $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-// Closing
-        curl_close($ch);
-
-// Will dump a beauty json :3
-        $result_array = json_decode($result, true);
-
-        if ($http_status != "200") {
-            if ($http_status == "404") {
-                Host::create(ip2long($ip), $ip, null);
-                echo "No data for this ip: $ipv4 <br>";
-                continue;
-            }
-            exit("Wrong HTTP Status Code: $http_status");
-        }
-
-        if (isset($result_array['error'])) {
-            // happens if "No information available for that IP."
-            exit("An error was detected.");
-        }
-
-
-        if (!isset($result_array['ip'])) {
-            // ip as integer 12345678
-            die("No IP given");
-        }
-
-
-        if (!Host::exist($result_array['ip'])) {
-            // insert if not exist
-            Host::create($result_array['ip'], $result_array['ip_str'], $result_array['last_update']);
-        }
-
-        $host = new Host($result_array['ip']);
-
-        if (isset($result_array['last_update'])) {
-            $host->updateLastUpdate($result_array['last_update']);
-        }
-
-        if (isset($result_array['ports'])) {
-            $host->updatePorts($result_array['ports']);
-        }
-
-        if (isset($result_array['vulns'])) {
-            $host->updateVulns($result_array['vulns']);
-        }
-
-        if (isset($result_array['tags'])) {
-            $host->updateTags($result_array['tags']);
-        }
-
-        if (isset($result_array['hostnames'])) {
-            $host->updateHostnames($result_array['hostnames']);
-        }
-
-        if (isset($result_array['country_code']) || isset($result_array['country_code3']) || isset($result_array['country_name'])) {
-            $host->updateCountry($result_array['country_code'], $result_array['country_code3'], $result_array['country_name']);
-        }
-
-        if (isset($result_array['os'])) {
-            $host->updateOS($result_array['os']);
-        }
-
-        if (isset($result_array['org'])) {
-            $host->updateORG($result_array['org']);
-        }
-
-        if (isset($result_array['asn'])) {
-            $host->updateASN($result_array['asn']);
-        }
-
-        if (isset($result_array['isp'])) {
-            $host->updateISP($result_array['isp']);
-        }
-
-        if (isset($result_array['latitude']) && isset($result_array['longitude'])) {
-            $host->updateLocation($result_array['latitude'], $result_array['longitude']);
-        }
-
-
+        scanIP($starttime, $ip);
     }
 }
 
